587edbab298aa78aaa30fc0ca1f92207d01cf7be
Aslam Hossain <38656636+aslamhossain43@users.noreply.github.com>
2024-08-25 09:25:53 +0600

    Add files via upload

---
+++ template/Cricket.java
@@ -0,0 +1,22 @@
+package behavioral.template;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Aug 25, 2024
+ */
+public class Cricket extends Game {
+    @Override
+    void endPlay() {
+        System.out.println("Cricket Game Finished!");
+    }
+
+    @Override
+    void initialize() {
+        System.out.println("Cricket Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Cricket Game Started. Enjoy the game!");
+    }
+}
