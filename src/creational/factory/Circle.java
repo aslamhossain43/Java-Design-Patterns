8547317141b028ee9b6c0c2b00b25a488b6f37d2
Aslam Hossain <38656636+aslamhossain43@users.noreply.github.com>
2024-08-28 12:32:02 +0600

    Add files via upload

---
+++ src/creational/factory/Circle.java
@@ -0,0 +1,13 @@
+package creational.factory;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Aug 28, 2024
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/creational/factory/Square.java
@@ -0,0 +1,13 @@
+package creational.factory;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Aug 28, 2024
+ */
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
